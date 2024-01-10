567021cf1cc235bed354ffc4a6bc5b64fb8a6b40
Marcelo Ferreira Miranda <marcelofmiranda23@gmail.com>
2024-01-10 18:29:44 -0300

    upando

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Traveling-to-CSharp
+Aqui é onde eu lhes apresento o início e possível meio da minha caminhada(afinal, o fim é uma bela incerteza).
---
+++ Study/Travel/Program.cs
@@ -0,0 +1,27 @@
+﻿// Fazendo uma matriz[4, 4] que aceita a entrada do Usuário para preenchimento de dados.
+
+string[,] matriz = new string[4,4];
+int x, y;
+
+for(x = 0; x < 4; x++)
+{
+    for(y = 0; y < 4; y++)
+    {
+       matriz[x, y] = (Console.ReadLine());
+      
+    }   
+}
+Console.WriteLine();
+
+for(x = 0; x < 4; x++)
+{
+    for(y = 0; y < 4; y++)
+    {
+        
+        Console.Write(matriz[x, y] + "   ");
+        
+    }
+    Console.WriteLine();
+}
+
+Console.ReadKey();
